083221db13d182b2f4550658547581cea0046c31
JJLizarazu05 <juanjoselizarazu2005@gmail.com>
2025-08-25 11:07:08 -0400

    Corregir funcionalidad dispositivo Xiaomi - Color StatusBar

--- app/src/main/java/com/jjlizarazu/infogram/LoginActivity.java
+++ app/src/main/java/com/jjlizarazu/infogram/LoginActivity.java
@@ -1,24 +1,28 @@
 package com.jjlizarazu.infogram;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
+        // Hacemos que la app ocupe toda la pantalla, dibujándose detrás de la barra de estado
+        androidx.core.view.WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Hacemos la barra de estado completamente transparente
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(android.graphics.Color.TRANSPARENT);
+        }
     }
 }
