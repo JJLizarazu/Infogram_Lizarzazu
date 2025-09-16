943032967c56a39e4747eb18c608f623f8f97b1c
JJLizarazu <juanjoselizarazu2005@gmail.com>
2025-09-15 20:34:11 -0400

    Fragments - 1.1

--- app/src/main/java/com/jjlizarazu/infogram/view/ContainerActivity.java
+++ app/src/main/java/com/jjlizarazu/infogram/view/ContainerActivity.java
@@ -1,14 +1,21 @@
  package com.jjlizarazu.infogram.view;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.jjlizarazu.infogram.R;
+import com.jjlizarazu.infogram.view.fragment.HomeFragment;
+import com.jjlizarazu.infogram.view.fragment.ProfileFragment;
+import com.jjlizarazu.infogram.view.fragment.SearchFragment;
 
  public class ContainerActivity extends AppCompatActivity {
 
@@ -22,5 +29,31 @@ import com.jjlizarazu.infogram.R;
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+
+                        if (item.getItemId() == R.id.search) {
+                            selectedFragment = new SearchFragment();
+                        } else if (item.getItemId() == R.id.home) {
+                            selectedFragment = new HomeFragment();
+                        } else if (item.getItemId() == R.id.profile) {
+                            selectedFragment = new ProfileFragment();
+                        }
+
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.container_frame, selectedFragment)
+                                .commit();
+
+                        return true;
+                    }
+                }
+        );
+
     }
 }
